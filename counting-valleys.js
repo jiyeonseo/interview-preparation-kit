f55877a557201bd32e943b347e03a5ec0a7678b7
seojeee <seojeee@gmail.com>
2018-10-28 16:27:16 +0900

    Create counting-valleys.js

---
+++ counting-valleys.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// https://www.hackerrank.com/challenges/counting-valleys/problem
+// Complete the countingValleys function below.
+function countingValleys(n, s) {
+  let valley = 0;
+  let level = 0;
+  const str = s.toString();
+    
+  for(let i = 0; i< str.length; i++) {
+      if(str[i] == "U") {
+          
+          level++;
+      }else {
+          if(level == 0){
+              valley ++;
+          }
+          level--;
+      }
+  } 
+    
+    return valley;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const s = readLine();
+
+    let result = countingValleys(n, s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
